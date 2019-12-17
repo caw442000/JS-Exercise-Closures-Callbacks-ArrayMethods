5de3ff30aba7465a9f1426828ccc82159a27e349
Cedric Winbush <cawinbushjr@gmail.com>
2019-12-17 14:15:21 -0600

    Complete Callbacks and Higher Order functions and array methods

--- index.js
+++ index.js
@@ -48,8 +48,9 @@ function processFirstItem(stringList, callback) {
  * [2] Invoking `processLength` passing `[]` and `(num) => "There are " + num`,
  * should return "There are 0".
 */
-function processLength(/* CODE HERE */) {
+function processLength(list, callback) {
   /* CODE HERE */
+  return callback(list.length);
 }
 
 /**
@@ -66,8 +67,9 @@ function processLength(/* CODE HERE */) {
  * Invoking `processLastItem` passing `['foo', 'bar']` and `(str) => str + str`,
  * should return 'barbar'.
 */
-function processLastItem(/* CODE HERE */) {
+function processLastItem(stringList, cb) {
   /* CODE HERE */
+  return cb(stringList[stringList.length-1])
 }
 
 /**
@@ -87,8 +89,13 @@ function processLastItem(/* CODE HERE */) {
  * [2] Invoking `processSum` passing `[]` and `(num) => num + 1000`,
  * should return 1000.
 */
-function processSum(/* CODE HERE */) {
+function processSum(numberList, callback) {
   /* CODE HERE */
+  const sumtotal= numberList.reduce(function(accumulator,currentvalue){
+    return accumulator + currentvalue
+  },0);
+
+  return callback(sumtotal);
 }
 
 /**
@@ -109,8 +116,12 @@ function processSum(/* CODE HERE */) {
  * [2] Invoking `processProduct` passing 25 and 0 and `(num) => num + 1000`,
  * should return 1000.
 */
-function processProduct(/* CODE HERE */) {
+function processProduct(num1, num2, callback) {
+  
   /* CODE HERE */
+  const num3 = num1 *num2;
+  return callback(num3);
+
 }
 
 /**
@@ -133,8 +144,11 @@ function processProduct(/* CODE HERE */) {
  * "lady gaga" and `['foo', 'bar']` and `(bool) => bool ? 'nice!' : 'sad'`,
  * should return "sad".
 */
-function processContains(/* CODE HERE */) {
+function processContains(item, list, callback) {
   /* CODE HERE */
+  const truth = list.includes(item);
+
+  return callback(truth);
 }
 
 /**
@@ -178,8 +192,14 @@ function processDuplicateFree(/* CODE HERE ONLY AFTER COMPLETING ALL OTHER TASKS
  * @returns an array with all the runners' full names in the following format: "Smith, John".
  * The full names appear in the array in the same order the runners appear in the `runners` array.
 */
-function getFullNames(/* CODE HERE */) {
+function getFullNames(runners) {
   /* CODE HERE */
+  let shirtOrder = [];
+  runners.forEach(function(items){
+    return shirtOrder.push(`${items.last_name}, ${items.first_name}`);
+  });
+
+  return shirtOrder;
 }
 
 /**
@@ -194,8 +214,15 @@ function getFullNames(/* CODE HERE */) {
  * @returns an array with all the runners' first names in ALL CAPS.
  * The first names appear in the array in the same order the runners appear in the `runners` array.
 */
-function firstNamesAllCaps(/* CODE HERE */) {
+function firstNamesAllCaps(runners) {
   /* CODE HERE */
+
+  const cappedName = runners.map(function(element){
+    return element.first_name.toUpperCase();  
+  }); 
+
+  return cappedName;
+
 }
 
 /**
@@ -211,8 +238,14 @@ function firstNamesAllCaps(/* CODE HERE */) {
  * @returns an array containing only the runners that use the given `tShirtSize`.
  * The runners in the array appear in the same order they appear in the `runners` array.
 */
-function getRunnersByTShirtSize(/* CODE HERE */) {
+function getRunnersByTShirtSize(runners, tShirtSize) {
   /* CODE HERE */
+
+  const tShirtFilter = runners.filter(function(item){
+    return item.shirt_size === tShirtSize
+  });
+
+  return tShirtFilter;
 }
 
 /**
@@ -225,8 +258,13 @@ function getRunnersByTShirtSize(/* CODE HERE */) {
  * @param runners array of runners like the one inside the /data/runners.js file.
  * @returns a number which is the sum of the donations by all runners.
 */
-function tallyUpDonations(/* CODE HERE */) {
+function tallyUpDonations(runners) {
   /* CODE HERE */
+  const totaldonations = runners.reduce(function(accumulator, currentValue){
+    return accumulator + currentValue.donation;
+  }, 0);
+
+  return totaldonations;
 }
 
 /////////////// CLOSURES ///////////////
